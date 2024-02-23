8a537bb938db49d99c4168b27270e40384932389
zazor <Svetoslav.Lazarov92@gmail.com>
2024-02-23 17:47:57 +0200

    Add BUILD_URL in playwright.yml

--- api/jira-api-payloads.ts
+++ api/jira-api-payloads.ts
@@ -1,8 +1,10 @@
+import { exec } from "child_process";
 import * as Configuration from "../configuration";
 
 const timestamp = new Date().toISOString();
-const buildNumber = process.env.BUILD_NUMBER || "LocalRun";
-const summary = `Test Execution [Automation] - ${timestamp} - Build: ${buildNumber}`;
+const buildNumber = process.env.BUILD_NUMBER || "local";
+const summary = `Test Execution [Automation] - ${timestamp} - Build: #${buildNumber}`;
+const execution = `Build URL - ${process.env.BUILD_URL || "N/A"}`;
 
 export const createExecutionBody = {
 	fields: {
@@ -10,7 +12,7 @@ export const createExecutionBody = {
 			key: Configuration.jira.projectKey,
 		},
 		summary: summary,
-		description: "Tests Execution [Automation]",
+		description: execution,
 		issuetype: {
 			name: "Test Execution",
 		},
