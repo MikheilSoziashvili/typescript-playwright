e2876ce71c8051ee297fcbcae70f4358dc7a2a16
mao1508 <marius.cirsteanc@gmail.com>
2024-02-26 21:55:46 +0200

    Place a single bet on Hilo and try to win - smoke test

---
+++ enums/hilo-result-messages.ts
@@ -0,0 +1,10 @@
+export enum HiloGameStatusMessage {
+    SPINNING_IN = "Spinning in",
+    DRAWING = "Drawing",
+}
+
+export enum HiloGameResultColor {
+    RED = "red",
+    BLACK = "black",
+    JOKER = "joker"
+}
