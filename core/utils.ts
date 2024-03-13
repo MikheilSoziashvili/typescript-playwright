97577a40b948335298d3e03cbde6ff65849cc783
zazor <Svetoslav.Lazarov92@gmail.com>
2024-03-13 17:09:28 +0200

    Revert parseFloat function in utils

--- core/utils.ts
+++ core/utils.ts
@@ -97,6 +97,9 @@ export function range(
 		.map((v) => start + v);
 }
 
+export function parseToFloat(num: number, fractionDigits: number = 2): string {
+    return parseFloat(`${num}`).toFixed(fractionDigits);
+
 export async function clearDirectoryContent(directory: string) {
 	try {
 		await fs
