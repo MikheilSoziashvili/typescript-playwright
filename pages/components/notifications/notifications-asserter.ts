a267083e87768ad49200345fe329cbd052ef9b42
mariusgmd <marius@teamgamdom.com>
2024-03-21 15:05:10 +0200

    Fix lint errors

--- pages/components/notifications/notifications-asserter.ts
+++ pages/components/notifications/notifications-asserter.ts
@@ -12,43 +12,33 @@ export class NotificationsAsserter extends BaseAsserter<Notifications> {
 		title: string,
 		options?: { index?: number },
 	): Promise<void> {
-		const titleText = await this.gamdomPage.map
-			.notificationTitleLocator(options)
-			.textContent();
-		expect(titleText).toEqual(title);
+		expect(
+			this.gamdomPage.map.notificationTitleLocator(options),
+		).toHaveText(title);
 	}
 
 	public async subTitleIs(
 		subTitle: string,
 		options?: { index?: number; title?: string },
 	): Promise<void> {
-		const subTitleText = await this.gamdomPage.map
-			.notificationSubTitleLocator(options)
-			.textContent();
-		expect(subTitleText).toEqual(subTitle);
+		expect(
+			this.gamdomPage.map.notificationSubTitleLocator(options),
+		).toHaveText(subTitle);
 	}
 
 	public async isDisplayed(options?: {
 		index?: number;
 		title?: string;
 	}): Promise<void> {
-		const isVisible = await this.gamdomPage.map
-			.notificationContainer(options)
-			.isVisible({
-				timeout: Timeout.SHORT,
-			});
-		expect(isVisible).toBe(true);
+		expect(
+			this.gamdomPage.map.notificationContainer(options),
+		).toBeVisible();
 	}
 
 	public async isNotDisplayed(options?: {
 		index?: number;
 		title?: string;
 	}): Promise<void> {
-		const isHidden = await this.gamdomPage.map
-			.notificationContainer(options)
-			.isHidden({
-				timeout: Timeout.SHORT,
-			});
-		expect(isHidden).toBe(true);
+		expect(this.gamdomPage.map.notificationContainer(options)).toBeHidden();
 	}
 }
