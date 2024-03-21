c2e8430681b9d2d3cfd57d642e7802dfccfaa351
mariusgmd <marius@teamgamdom.com>
2024-03-21 14:40:32 +0200

    Add notification validation in affiliates test

--- tests/affiliates.spec.ts
+++ tests/affiliates.spec.ts
@@ -14,6 +14,7 @@ test.describe("Affiliates tests", () => {
 		profilePage,
 		rewardsPage,
 		faqPage,
+		notifications,
 	}) => {
 		await homePage.navigateAndCheckTitle();
 		await homePage.openRegisterModal();
@@ -51,7 +52,15 @@ test.describe("Affiliates tests", () => {
 		await rewardsPage.navigate();
 		await rewardsPage.steps().claimCode(AUTOMATION_AFFILIATES_CODE);
 
-		//TODO: Add assertion for toast when components are implemented QT-334 and QT-335
+		await notifications.assertThat().isDisplayed();
+		await notifications
+			.assertThat()
+			.titleIs(NotificationsTitles.WELCOME_BONUS);
+		await notifications.aknowledge({
+			title: NotificationsTitles.WELCOME_BONUS,
+		});
+		await notifications.assertThat().isNotDisplayed();
+		//TODO: Add assertion for toast when components are implemented QT-334
 
 		await faqPage.navigate();
 		await faqPage.expandAffiliateCodeRegisteredUnderSection();
