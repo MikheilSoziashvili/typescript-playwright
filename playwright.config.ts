4878d462bef747919b57f9a1c44af8809744cdb7
mariusgmd <marius@teamgamdom.com>
2024-04-16 19:01:19 +0300

    Fix tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,8 +51,6 @@ export default defineConfig({
 		trace: "on-first-retry",
 
 		launchOptions: {
-			// Browser proxy option is required for Chromium on Windows.
-			proxy: { server: "per-context" },
 			slowMo: 300,
 		},
 
