59264e5dd5f6f975090c01dac5756d82566eb85c
angel5rov <angel@teamgamdom.com>
2024-07-05 14:26:14 +0300

    Update jira-reporter
    additional refactoring of methods, also adding a case where report was not uploaded when test is failed

--- core/reporters/jira-reporter/jira-reporter.ts
+++ core/reporters/jira-reporter/jira-reporter.ts
@@ -1,4 +1,5 @@
-import { Reporter } from "@playwright/test/reporter";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Reporter, FullResult } from "@playwright/test/reporter";
 import { XrayApi } from "@api/xray-api";
 import { readFromJSONFile } from "@core/utils";
 import { logger } from "@logger/logger";
@@ -7,41 +8,37 @@ import * as Configuration from "configuration";
 import { parseXmlFile, updateXmlWithTestKeys } from "@core/xml-utils";
 
 export default class JiraReporter implements Reporter {
-	private async addTestKeysToXmlReport(filePath: string): Promise<void> {
-		const xmlData = await parseXmlFile(filePath);
-		await updateXmlWithTestKeys(filePath, xmlData);
-	}
+	async onEnd(result: FullResult): Promise<void> {
+		const keystore = await readFromJSONFile(Configuration.keystore);
+		const issueKey = keystore.issueKey as string;
+		const createExecution = keystore.createExecution as boolean;
 
-	private async handleTestResults(
-		issueKey: string,
-		createExecution: boolean,
-	): Promise<void> {
-		if (createExecution && issueKey) {
-			await this.addTestKeysToXmlReport("./results.xml");
-			logger.info(`Test execution key is ${issueKey}`);
-			logger.info("Uploading XML report...");
-
-			const xrayApi = new XrayApi();
-			await xrayApi.initialize();
-
-			const response = await xrayApi.importXmlResult(issueKey);
-
-			expect(response.status()).toBe(200);
-			logger.info(
-				`Report is uploaded successfully in Test Execution ${issueKey}`,
-			);
+		// Upload test results if there are failed tests or if execution creation is requested
+		if (result.status === "failed" || (createExecution && issueKey)) {
+			await this.uploadTestResults(issueKey);
 		} else {
 			logger.info("Test results are not imported into XRay.");
 		}
 	}
 
-	async onExit(): Promise<void> {
-		const keystore = await readFromJSONFile(Configuration.keystore);
-		const issueKey = keystore.issueKey as string;
+	private async uploadTestResults(issueKey: string): Promise<void> {
+		await this.addTestKeysToXmlReport("./results.xml");
+		logger.info(`Test execution key is ${issueKey}`);
+		logger.info("Uploading XML report...");
 
-		await this.handleTestResults(
-			issueKey,
-			keystore.createExecution as boolean,
+		const xrayApi = new XrayApi();
+		await xrayApi.initialize();
+
+		const response = await xrayApi.importXmlResult(issueKey);
+		expect(response.status()).toBe(200);
+
+		logger.info(
+			`Report is uploaded successfully in Test Execution ${issueKey}`,
 		);
+	}
+
+	private async addTestKeysToXmlReport(filePath: string): Promise<void> {
+		const xmlData = await parseXmlFile(filePath);
+		await updateXmlWithTestKeys(filePath, xmlData);
 	}
 }
