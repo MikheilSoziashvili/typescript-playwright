367ef3d51f9d5d97cf92c05af175cdabf5e4a5d9
angel5rov <angel@teamgamdom.com>
2024-07-10 15:34:09 +0300

    Refactor mailinator-api
    Refactored mailinator-api
    Moved mailinator interfaces into /core/api/interfaces/mailinator-interfaces.ts

--- api/mailinator-api.ts
+++ api/mailinator-api.ts
@@ -4,45 +4,26 @@ import {
 } from "@constants/mailinator-endpoints";
 import { BaseApi } from "./base-api";
 import * as Configuration from "../configuration";
-
-interface MessagePart {
-	body: string;
-}
-
-interface Message {
-	id: string;
-	subject: string;
-	parts: MessagePart[];
-}
-
-interface MessagesResponse {
-	msgs: Message[];
-}
-
-interface EmailResponse {
-	id: string;
-	from: string;
-	seconds_ago: number;
-	headers: Record<string, string>;
-}
+import {
+	Message,
+	MessagesResponse,
+	EmailResponse,
+} from "../core/api/interfaces/mailinator-interfaces";
 
 export class MailinatorApi extends BaseApi {
-	private headers: Record<string, string> = {};
-
 	constructor(
 		mailinatorConfig: Record<string, string> = Configuration.mailinator,
 	) {
 		super(mailinatorConfig.baseUrl);
-		this.headers = {
+		this.setHeaders({
 			Authorization: `Bearer ${mailinatorConfig.apiKey}`,
 			"Content-Type": "application/json",
-		};
+		});
 	}
 
 	private async fetchMailinator<T>(endpoint: string): Promise<T> {
 		const response = await this.get({
 			endpoint: endpoint,
-			headers: this.headers,
 		});
 
 		if (!response.ok()) {
@@ -54,26 +35,22 @@ export class MailinatorApi extends BaseApi {
 		return response.json() as T;
 	}
 
-	async getMessages(): Promise<Message[]> {
-		const messagesResponse: MessagesResponse =
-			await this.fetchMailinator<MessagesResponse>(
-				MAILINATOR_MESSAGES_ENDPOINT,
-			);
+	public async getMessages(): Promise<Message[]> {
+		const messagesResponse = await this.fetchMailinator<MessagesResponse>(
+			MAILINATOR_MESSAGES_ENDPOINT,
+		);
 
-		return messagesResponse["msgs"];
+		return messagesResponse.msgs;
 	}
 
-	async countMessages(): Promise<number> {
+	public async countMessages(): Promise<number> {
 		const messages = await this.getMessages();
-
 		return messages.length;
 	}
 
-	async getEmail(emailId: string): Promise<EmailResponse> {
-		const emailResponse: EmailResponse = await this.fetchMailinator(
+	public async getEmail(emailId: string): Promise<EmailResponse> {
+		return this.fetchMailinator<EmailResponse>(
 			`${MAILINATOR_EMAIL_ENDPOINT}/${emailId}`,
 		);
-
-		return emailResponse;
 	}
 }
---
+++ core/api/interfaces/mailinator-interfaces.ts
@@ -0,0 +1,20 @@
+export interface MessagePart {
+	body: string;
+}
+
+export interface Message {
+	id: string;
+	subject: string;
+	parts: MessagePart[];
+}
+
+export interface MessagesResponse {
+	msgs: Message[];
+}
+
+export interface EmailResponse {
+	id: string;
+	from: string;
+	seconds_ago: number;
+	headers: Record<string, string>;
+}
