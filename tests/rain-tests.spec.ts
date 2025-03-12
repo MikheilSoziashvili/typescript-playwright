79fc4ba19c8ca9a06fddb4c76f69240578ec8601
angel5rov <angel@teamgamdom.com>
2025-03-12 10:24:05 +0200

    [ENG-5468] [QA Automation] Adjust rain tests logic and handling (#268)

--- tests/rain-tests.spec.ts
+++ tests/rain-tests.spec.ts
@@ -1,37 +1,51 @@
-import { getCookieHeader, waitUntil } from "@core/utils/utils";
+import { NL_PROXY_CREDENTIALS } from "@constants/proxies";
+import { TIP_RAIN } from "@constants/tip-rain";
+import { buildTipRainUserMessageInfo } from "@core/helpers/asserter-helpers/text-asserters";
+import {
+	createBrowserContextWithProxy,
+	createPngImagePath,
+	deleteFilesWithFilePaths,
+	getCookieHeader,
+	initializePageObjects,
+	initializePageObjectsWithCookies,
+	waitUntil,
+} from "@core/utils/utils";
 import { RegisterTestData } from "@dtos/test-data";
 import { TimeoutSeconds } from "@enums/timeout-seconds";
 import { storageStateNewUserAPI } from "@fixtures/auth-fixtures";
 import { test } from "@fixtures/fixtures";
 
-test.describe("Rain tests", () => {
-	test.slow();
-
-	const baseRainAmount = 0.67;
-
-	test.beforeEach(async ({ gamdomApi }) => {
-		const superAdminData = new RegisterTestData({
-			useGamdomEmailDomain: true,
-		});
-		const superAdminCookie = getCookieHeader(
-			await gamdomApi.authenticateWithNewSuperAdminUser(superAdminData),
-		);
-
-		await gamdomApi.ensureRainExists({
-			active: true,
-			extraAmount: 1000,
-			frequencyMins: 1,
-			maxAmount: 1000,
-			minAmount: 1000,
-			percentExtraAmount: 5,
-			headers: {
-				Cookie: superAdminCookie,
-			},
-		});
+test.beforeEach(async ({ gamdomApi }) => {
+	const superAdminData = new RegisterTestData({
+		useGamdomEmailDomain: true,
 	});
 
-	test.use(storageStateNewUserAPI());
+	const superAdminCookie = getCookieHeader(
+		await gamdomApi.authenticateWithNewSuperAdminUser(superAdminData),
+	);
 
+	await gamdomApi.stopCustomRain({
+		Cookie: superAdminCookie,
+	});
+
+	await gamdomApi.ensureRainExists({
+		active: true,
+		extraAmount: 1000,
+		frequencyMins: 1,
+		maxAmount: 1000,
+		minAmount: 1000,
+		percentExtraAmount: 5,
+		headers: {
+			Cookie: superAdminCookie,
+		},
+	});
+});
+
+test.describe("Rain claim tests", () => {
+	test.slow();
+	const baseRainAmount = 0.67;
+
+	test.use(storageStateNewUserAPI());
 	test("[ENG-2863] Rain - try to claim the rain", async ({
 		homePage,
 		chat,
@@ -68,5 +82,88 @@ test.describe("Rain tests", () => {
 		await homePage.authenticatedHeader
 			.assertThat()
 			.accountBalanceIs(expectedUserBalance);
+	});
+});
+
+test.describe("Tip rain tests", () => {
+	let qrCode2FAImagePath: string;
+	const tipRainAmount = 10;
+	const userData = new RegisterTestData();
+
+	test.use(
+		storageStateNewUserAPI({
+			username: userData.username,
+			password: userData.password,
+			email: userData.email,
+		}),
+	);
+	test("[ENG-2564] Tip rain - Require new 2FA code when IP of user changes", async ({
+		homePage,
+		chat,
+		tipRainModal,
+		twoFactorAuthModal,
+		settingsPage,
+		browser,
+	}) => {
+		const pages = {
+			homePage,
+			tipRainModal,
+			twoFactorAuthModal,
+			settingsPage,
+			chat,
+		};
+		const initialPage = await initializePageObjects(
+			await browser.newContext(),
+			...Object.values(pages),
+		);
+
+		qrCode2FAImagePath = createPngImagePath();
+		await settingsPage
+			.steps()
+			.navigateAndEnable2FaAuthentication(qrCode2FAImagePath);
+
+		await homePage.navigate();
+		await homePage.authenticatedHeader.expandChatIfNotVisible();
+		await chat.steps().verifyChatAndSendMessage(TIP_RAIN);
+		await twoFactorAuthModal
+			.steps()
+			.generateAndEnter2FaCodeSuccessfully(qrCode2FAImagePath);
+		await tipRainModal
+			.steps()
+			.verifyModalAndTipRainSuccessfully(tipRainAmount);
+		await chat.assertThat().isInfoMessageVisible(
+			buildTipRainUserMessageInfo({
+				username: userData.username,
+				tipRainAmount: tipRainAmount,
+			}),
+		);
+
+		await chat.steps().verifyChatAndSendMessage(TIP_RAIN);
+		await twoFactorAuthModal.assertThat().modal2FaNotDisplayed();
+
+		await initializePageObjectsWithCookies(
+			await (await browser.newContext()).cookies(),
+			initialPage,
+			await createBrowserContextWithProxy(browser, NL_PROXY_CREDENTIALS),
+			...Object.values(pages),
+		);
+
+		await homePage.navigate();
+		await homePage.authenticatedHeader.expandChatIfNotVisible();
+		await chat.steps().verifyChatAndSendMessage(TIP_RAIN);
+		await twoFactorAuthModal
+			.steps()
+			.generateAndEnter2FaCodeSuccessfully(qrCode2FAImagePath);
+		await tipRainModal
+			.steps()
+			.verifyModalAndTipRainSuccessfully(tipRainAmount);
+		await chat.assertThat().isInfoMessageVisible(
+			buildTipRainUserMessageInfo({
+				username: userData.username,
+				tipRainAmount: tipRainAmount,
+			}),
+		);
+
+		await deleteFilesWithFilePaths([qrCode2FAImagePath]);
 	});
 });
