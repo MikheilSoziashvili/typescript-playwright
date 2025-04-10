51e0689077730938f0fd7b32dc29ce4c87388aef
ralucaariton <59613902+ralucaariton@users.noreply.github.com>
2025-04-10 15:35:40 +0300

    [ENG-6047] Fix failing tests for Dice (#313)

--- pages/components/header/unauthenticated/unauthenticated-header-asserter.ts
+++ pages/components/header/unauthenticated/unauthenticated-header-asserter.ts
@@ -1,7 +1,6 @@
 import { BaseAsserter } from "@base/base-asserter";
-import { UnauthenticatedHeader } from "./unauthenticated-header";
 import { Timeout } from "@enums/timeout";
-import { expect } from "@playwright/test";
+import { UnauthenticatedHeader } from "./unauthenticated-header";
 
 export class UnauthenticatedHeaderAsserter extends BaseAsserter<UnauthenticatedHeader> {
 	public constructor(unauthenticatedHeader: UnauthenticatedHeader) {
