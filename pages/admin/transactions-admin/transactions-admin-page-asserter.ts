f35a3e7f73a761c0847efd3a66aafb136d3f20f6
Niksh0 <57258887+Niksh0@users.noreply.github.com>
2025-06-30 17:46:39 +0300

    [ENG-7353] [QA Automation] [Flaky Test] Promo Codes - Verify that Promo Win Cash & Promo Win Free Spins log filters are working (#411)

--- pages/admin/transactions-admin/transactions-admin-page-asserter.ts
+++ pages/admin/transactions-admin/transactions-admin-page-asserter.ts
@@ -2,6 +2,8 @@ import { BaseAsserter } from "@base/base-asserter";
 import { TransactionsAdminPage } from "./transactions-admin-page";
 import { expect } from "@playwright/test";
 import { step } from "decorators/step";
+import { waitUntil } from "@core/utils/utils";
+import { logger } from "@logger/logger";
 
 export class TransactionsAdminPageAsserter extends BaseAsserter<TransactionsAdminPage> {
 	public constructor(page: TransactionsAdminPage) {
@@ -14,18 +16,26 @@ export class TransactionsAdminPageAsserter extends BaseAsserter<TransactionsAdmi
 	): Promise<void> {
 		await this.checkElementsAreVisible([this.gamdomPage.map.logsTableBody]);
 
-		const actualValues =
-			await this.gamdomPage.map.logsTableTransactionDetailsColumn.allTextContents();
+		let actualValues: string[] = [];
 
-		expect(
-			actualValues.length,
-			`Mismatch in the number of rows. Expected: ${expectedValues.length}, Actual: ${actualValues.length}`,
-		).toBe(expectedValues.length);
+		await waitUntil(
+			async () => {
+				actualValues =
+					await this.gamdomPage.map.logsTableTransactionDetailsColumn.allTextContents();
+					logger.info(`Waiting for table rows. Got: ${actualValues.length}`);
+				return actualValues.length === expectedValues.length;
+			},
+			{
+				errorMessage: `Mismatch in the number of rows. Expected: ${expectedValues.length}, Actual: ${actualValues.length}`,
+				intervalSeconds: 1,
+				timeoutSeconds: 5,
+			},
+		);
 
 		const missingValues = expectedValues.filter(
 			(value) => !actualValues.includes(value),
 		);
-
+		
 		expect(
 			missingValues,
 			`The following expected values were not found in the transaction details column: ${missingValues.join(
