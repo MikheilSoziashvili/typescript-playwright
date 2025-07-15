0ea7fcc3930a6e07507ae3e94bcb6624e6132d1a
ralucaariton <59613902+ralucaariton@users.noreply.github.com>
2025-07-15 11:02:03 +0300

    [NO_TICKET] Remove fixme and add annotation in Hilo game statistics test (#437)
    Until now we had fixme over those tests (that are failing now), but we discussed about this and agreed to remove fixme and let the tests to fail, with the appropriate annotation with Jira bug, so the failing behaviour is ok in this case

--- tests/hilo-game-statistics.spec.ts
+++ tests/hilo-game-statistics.spec.ts
@@ -1,8 +1,9 @@
 import { DATASETS_DIR } from "@constants/file-paths";
-import { parse_csv } from "@core/utils/utils";
+import { jiraIssueId, parse_csv } from "@core/utils/utils";
 import { CsvFilesName } from "@enums/csv-file-name";
 import { HiloGameStatusMessage } from "@enums/hilo-result-messages";
 import { OriginalGame } from "@enums/original-games";
+import { AnnotationType } from "@enums/playwright/annotationsTypes";
 import { storageStateNewUserDB } from "@fixtures/auth-fixtures";
 import { test } from "@fixtures/fixtures";
 
@@ -18,46 +19,48 @@ test.describe("Hilo game - statistics tests", () => {
 	test.slow();
 
 	lastRoundsInputs.forEach((input) => {
-		test(`[ENG-2138] Verify last '${input.lastRounds}' rounds Red/Black percentage history statistics @originals`, async ({
-			hiloGamePage,
-			originalsPage,
-		}) => {
-			test.fixme(
-				true,
-				"Issue [ENG-4718] The Stats calculation for Last x rounds is wrong",
-			);
-			await hiloGamePage.navigate();
-			await originalsPage.waitForGameRoundFinish(OriginalGame.HiLo);
-			await hiloGamePage
-				.assertThat()
-				.gameMessageIs(HiloGameStatusMessage.SPINNING_IN);
+		test(
+			`[ENG-2138] Verify last '${input.lastRounds}' rounds Red/Black percentage history statistics @originals`,
+			{
+				annotation: {
+					type: AnnotationType.BUG,
+					description: jiraIssueId(4718),
+				},
+			},
+			async ({ hiloGamePage, originalsPage }) => {
+				await hiloGamePage.navigate();
+				await originalsPage.waitForGameRoundFinish(OriginalGame.HiLo);
+				await hiloGamePage
+					.assertThat()
+					.gameMessageIs(HiloGameStatusMessage.SPINNING_IN);
 
-			await hiloGamePage.steps().openHistoryModalSuccessfully();
+				await hiloGamePage.steps().openHistoryModalSuccessfully();
 
-			const hiloCardsColorDataHistoryModal = await hiloGamePage
-				.steps()
-				.getHistoryModalCardsPercentageValue(input.lastRounds);
+				const hiloCardsColorDataHistoryModal = await hiloGamePage
+					.steps()
+					.getHistoryModalCardsPercentageValue(input.lastRounds);
 
-			await hiloGamePage.steps().closeHistoryModalSuccessfully();
-			await hiloGamePage
-				.steps()
-				.selectLastRoundsDropdownValues(input.lastRounds);
-			const hiloCardsColorDataStatsArea = await hiloGamePage
-				.steps()
-				.getStatsAreaCardsProbabilityPercentageValue();
+				await hiloGamePage.steps().closeHistoryModalSuccessfully();
+				await hiloGamePage
+					.steps()
+					.selectLastRoundsDropdownValues(input.lastRounds);
+				const hiloCardsColorDataStatsArea = await hiloGamePage
+					.steps()
+					.getStatsAreaCardsProbabilityPercentageValue();
 
-			await hiloGamePage
-				.assertThat()
-				.cardColorPercentageValuesEqual(
-					hiloCardsColorDataHistoryModal.blackCards,
-					hiloCardsColorDataStatsArea.blackCards,
-				);
-			await hiloGamePage
-				.assertThat()
-				.cardColorPercentageValuesEqual(
-					hiloCardsColorDataHistoryModal.redCards,
-					hiloCardsColorDataStatsArea.redCards,
-				);
-		});
+				await hiloGamePage
+					.assertThat()
+					.cardColorPercentageValuesEqual(
+						hiloCardsColorDataHistoryModal.blackCards,
+						hiloCardsColorDataStatsArea.blackCards,
+					);
+				await hiloGamePage
+					.assertThat()
+					.cardColorPercentageValuesEqual(
+						hiloCardsColorDataHistoryModal.redCards,
+						hiloCardsColorDataStatsArea.redCards,
+					);
+			},
+		);
 	});
 });
