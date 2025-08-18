11e2827c1595a4bc7d66acd72427b00f21130030
Niksh0 <57258887+Niksh0@users.noreply.github.com>
2025-08-18 16:47:38 +0300

    [ENG-8195] Add bug annotation for Tip user from the chat test (#493)

--- tests/tip-user.spec.ts
+++ tests/tip-user.spec.ts
@@ -4,6 +4,7 @@ import { ChatMessageOptions } from "@components/chat/chat-map";
 import {
 	encodeCookieHeader,
 	generateRandomString,
+	jiraIssueId,
 	setAuthenticationCookies,
 } from "@core/utils/utils";
 import { USER_2_CREDENTIALS, USER_3_CREDENTIALS } from "@constants/credentials";
@@ -15,6 +16,7 @@ import {
 import { Unit } from "@enums/units";
 import { WalletType } from "@enums/wallet-types";
 import { Currency } from "@enums/currencies";
+import { AnnotationType } from "@enums/playwright/annotationsTypes";
 
 test.describe("Tip user tests", () => {
 	const message_1 = generateRandomString({ prefix: "automation_message_" });
@@ -44,92 +46,94 @@ test.describe("Tip user tests", () => {
 			await homePage.authenticatedHeader.getAccountBalance();
 	});
 
-	test("[ENG-290] 'Tip User' from the chat @smoke", async ({
-		homePage,
-		gamdomApi,
-		chat,
-		tipUserModal,
-		toast,
-		page,
-	}) => {
-		test.slow();
-		await homePage.navigate({ cookies: { clearCookies: true } });
-		await homePage.authenticatedHeader.expandChatIfNotVisible();
-		await chat.assertThat().chatIsDisplayed();
-		await chat
-			.assertThat()
-			.isPlaceholderVisible(ChatFooterPlaceholder.LOGIN_TO_CHAT);
+	test(
+		"[ENG-290] 'Tip User' from the chat @smoke",
+		{
+			annotation: {
+				type: AnnotationType.BUG,
+				description: jiraIssueId(8193),
+			},
+		},
+		async ({ homePage, gamdomApi, chat, tipUserModal, toast, page }) => {
+			test.slow();
+			await homePage.navigate({ cookies: { clearCookies: true } });
+			await homePage.authenticatedHeader.expandChatIfNotVisible();
+			await chat.assertThat().chatIsDisplayed();
+			await chat
+				.assertThat()
+				.isPlaceholderVisible(ChatFooterPlaceholder.LOGIN_TO_CHAT);
 
-		const cookie = await gamdomApi.authenticateWithExistingUser(
-			USER_2_CREDENTIALS.username,
-			USER_2_CREDENTIALS.password,
-		);
-		const headersUser2 = {
-			Cookie: await encodeCookieHeader(cookie),
-		};
-		await setAuthenticationCookies(page, cookie);
-
-		await homePage.navigate();
-		await chat.assertThat().chatIsDisplayed();
-		await chat.assertThat().isMessageVisible(messageInfo_1);
-
-		const user2AccountBalance =
-			await homePage.authenticatedHeader.getAccountBalance();
-
-		await chat.steps().openTipUserModal(messageInfo_1);
-		await tipUserModal.insertTipValue(tipValue);
-		await tipUserModal.assertThat().isValueVisible(tipValue);
-
-		await tipUserModal.clearTipValue();
-		await tipUserModal.assertThat().isValueVisible(0);
-
-		await tipUserModal.tipUser(tipValue);
-
-		await toast.assertThat().titleIs(ToastTitle.SUCCESS);
-		await toast.assertThat().subTitleIs(
-			buildTipUserSubTitle({
-				username: messageInfo_1.username,
-				tipAmount: tipValue,
-			}),
-		);
-		await chat.assertThat().isInfoMessageVisible(
-			buildTipUserMessageInfo({
-				senderUsername: USER_2_CREDENTIALS.username,
-				receiverUsername: USER_3_CREDENTIALS.username,
-				tipAmount: tipValue,
-			}),
-		);
-		await homePage.authenticatedHeader
-			.assertThat()
-			.accountBalanceIs(
-				user2AccountBalance - Number(tipValue),
-				Unit.COINS,
-				Currency.USD,
-				WalletType.DEFAULT,
-				headersUser2,
+			const cookie = await gamdomApi.authenticateWithExistingUser(
+				USER_2_CREDENTIALS.username,
+				USER_2_CREDENTIALS.password,
 			);
-		await chat
-			.assertThat()
-			.isPlaceholderVisible(ChatFooterPlaceholder.START_TYPING);
+			const headersUser2 = {
+				Cookie: await encodeCookieHeader(cookie),
+			};
+			await setAuthenticationCookies(page, cookie);
 
-		await homePage.navigate({ cookies: { clearCookies: true } });
-		const cookieUser3 = await gamdomApi.authenticateWithExistingUser(
-			USER_3_CREDENTIALS.username,
-			USER_3_CREDENTIALS.password,
-		);
-		const headersUser3 = {
-			Cookie: await encodeCookieHeader(cookieUser3),
-		};
-		await setAuthenticationCookies(page, cookieUser3);
-		await homePage.navigate();
-		await homePage.authenticatedHeader
-			.assertThat()
-			.accountBalanceIs(
-				user3AccountBalance + Number(tipValue),
-				Unit.COINS,
-				Currency.USD,
-				WalletType.DEFAULT,
-				headersUser3,
+			await homePage.navigate();
+			await chat.assertThat().chatIsDisplayed();
+			await chat.assertThat().isMessageVisible(messageInfo_1);
+
+			const user2AccountBalance =
+				await homePage.authenticatedHeader.getAccountBalance();
+
+			await chat.steps().openTipUserModal(messageInfo_1);
+			await tipUserModal.insertTipValue(tipValue);
+			await tipUserModal.assertThat().isValueVisible(tipValue);
+
+			await tipUserModal.clearTipValue();
+			await tipUserModal.assertThat().isValueVisible(0);
+
+			await tipUserModal.tipUser(tipValue);
+
+			await toast.assertThat().titleIs(ToastTitle.SUCCESS);
+			await toast.assertThat().subTitleIs(
+				buildTipUserSubTitle({
+					username: messageInfo_1.username,
+					tipAmount: tipValue,
+				}),
 			);
-	});
+			await chat.assertThat().isInfoMessageVisible(
+				buildTipUserMessageInfo({
+					senderUsername: USER_2_CREDENTIALS.username,
+					receiverUsername: USER_3_CREDENTIALS.username,
+					tipAmount: tipValue,
+				}),
+			);
+			await homePage.authenticatedHeader
+				.assertThat()
+				.accountBalanceIs(
+					user2AccountBalance - Number(tipValue),
+					Unit.COINS,
+					Currency.USD,
+					WalletType.DEFAULT,
+					headersUser2,
+				);
+			await chat
+				.assertThat()
+				.isPlaceholderVisible(ChatFooterPlaceholder.START_TYPING);
+
+			await homePage.navigate({ cookies: { clearCookies: true } });
+			const cookieUser3 = await gamdomApi.authenticateWithExistingUser(
+				USER_3_CREDENTIALS.username,
+				USER_3_CREDENTIALS.password,
+			);
+			const headersUser3 = {
+				Cookie: await encodeCookieHeader(cookieUser3),
+			};
+			await setAuthenticationCookies(page, cookieUser3);
+			await homePage.navigate();
+			await homePage.authenticatedHeader
+				.assertThat()
+				.accountBalanceIs(
+					user3AccountBalance + Number(tipValue),
+					Unit.COINS,
+					Currency.USD,
+					WalletType.DEFAULT,
+					headersUser3,
+				);
+		},
+	);
 });
