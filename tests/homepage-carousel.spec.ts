71d113631c44a2d0a46808bd7ab6616fe61c37c6
ralucaariton <59613902+ralucaariton@users.noreply.github.com>
2025-09-08 12:57:34 +0300

    [NO-TICKET] Add OBT annotation for esports banner (#545)

--- tests/homepage-carousel.spec.ts
+++ tests/homepage-carousel.spec.ts
@@ -3,6 +3,8 @@ import { storageStateNewUserDB } from "@fixtures/auth-fixtures";
 import { HomePageBannerCarouselSlideTitle } from "@enums/homepage-banner-carousel-slide-title";
 import { testDetails } from "@core/helpers/test-details-helper";
 import { JiraUser } from "@enums/jira/jira-users";
+import { isScheduledRun } from "configuration";
+import { TestTag } from "@enums/test-tags";
 
 // NOTE: For Usain Bolt, Black Jack and Drop and wins slides should be implemented when ENG-1525 is fixed
 
@@ -40,8 +42,13 @@ test.describe("Homepage banner carousel", () => {
 
 	test(
 		`[ENG-1158] - Homepage banner carousel - esports page`,
-		testDetails().withAuthor(JiraUser.IVAYLO_STOYCHEV).apply(),
+		testDetails()
+			.withTags(TestTag.LOCAL)
+			.withJiraBugTickets("8283")
+			.withAuthor(JiraUser.IVAYLO_STOYCHEV)
+			.apply(),
 		async ({ homePage, esportsPage }) => {
+			test.fixme(isScheduledRun);
 			await homePage.navigateAndCheckTitle();
 			await homePage.assertThat().isBannerCarouselDisplayed();
 			await homePage
