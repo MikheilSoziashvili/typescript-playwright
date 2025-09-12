10fb7dc538e1ae6cf7a0aa38d962235954628bb0
Niksh0 <57258887+Niksh0@users.noreply.github.com>
2025-09-12 10:45:55 +0300

    [ENG-8628] Crash autobet - check for visibility for the countdown timer (#553)

--- pages/crash-game-page/crash-game-page.ts
+++ pages/crash-game-page/crash-game-page.ts
@@ -125,6 +125,10 @@ export class CrashGamePage extends BasePage<CrashGamePageMap> {
 
 	@step("Get countdown timer")
 	public async getCountdownTimer(): Promise<string> {
+		await this.assertThat().checkElementsAreVisible(
+			[this.map.spinningCountdownCounter],
+			Timeout.EXTRA_MAX / 2,
+		);
 		const countdownTimerText =
 			await this.map.spinningCountdownCounter.innerText();
 		return countdownTimerText;
