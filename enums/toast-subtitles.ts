67cf6b951d5d40cb11a304897208fee781561ee3
Niksh0 <57258887+Niksh0@users.noreply.github.com>
2025-10-07 04:51:58 -0700

    [ENG-9204] Create KOTH event - edit toast subtitle (#599)

--- enums/toast-subtitles.ts
+++ enums/toast-subtitles.ts
@@ -29,7 +29,7 @@ export enum ToastSubTitle {
 	SUCCESSFUL_EDIT = "Edit successful!",
 	ONLY_DECREASE_WALLET_AMOUNT = "Sorry, You only can decrease the wallet amount!",
 	SELF_EXCLUSION = "You cannot bet when you are in self exclusion.",
-	KOTH_EVENT_CREATED = "Event started/scheduled successfully!",
+	KOTH_EVENT_CREATED = "Event started successfully!",
 	START_IN_PAST = "Start date must not be in the past.",
 	EXPIRY_BEFORE_START = "Expiry date must be after start date",
 	PROCESSED_OK = "Rewards hand outs processed, please check the logs below!",
