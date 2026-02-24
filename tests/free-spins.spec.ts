3583e11d56d257715ab1629fd8192288b5a9b6ae
ralucaariton <59613902+ralucaariton@users.noreply.github.com>
2026-02-24 11:30:07 +0200

    [NO-TICKET] Remove fixme from free spins test (#911)

--- tests/free-spins.spec.ts
+++ tests/free-spins.spec.ts
@@ -81,12 +81,10 @@ test.describe("Free spins tests", () => {
 					test(
 						`[ENG-5008] Granting free spins in batch - ${description}`,
 						testDetails()
-							.withJiraBugTickets("15023")
 							.withTags(JiraComponent.FREE_SPINS)
 							.withAuthor(JiraUser.RALUCA_ARITON)
 							.apply(),
 						async ({ freeSpinsAdminPage }) => {
-							test.fixme(true);
 							await freeSpinsAdminPage.navigate();
 							await freeSpinsAdminPage
 								.steps()
